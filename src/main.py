9e4c7ccea3353eef243c1188df39058ce72ee135
Thiago Monteiro <tsmonteiro@gmail.com>
2023-08-28 18:17:47 +0000

    remove relative import

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from tercen.client import context as ctx
 import numpy as np
-from . import calc_mean
+from operator_funcs import calc_mean
 
 tercenCtx = ctx.TercenContext()
 
-df = calc_mean(tercenCtx)
+df = calc_mean(tercenCtxteRcen)
 
 df = tercenCtx.add_namespace(df) 
 tercenCtx.save(df)
